c1b308c6c48da594446abcbd0f40a4801553327c
IvanM <ivan.markovic1402@gmail.com>
2020-07-28 15:17:34 +0200

    small change

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://pivdza:hamkam5525@ivan.oijgi.mongodb.net/shoppinglist?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://@ivan.oijgi.mongodb.net/shoppinglist?retryWrites=true&w=majority'
 }
